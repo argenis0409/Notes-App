d2253bc7769113bc57aa553324b26913570bc121
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-21 20:14:26 -0800

    updated the editor component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
 
 export default class Editor extends Component {
-    render() {
+    constructor () {
+        super();
+        this.state = {
+          text: '',
+          color: '#FFA726'
+        }
+      }
+      
+      handleTextChange = (event) => {
+        this.setState({ text: event.target.value });
+      };
+    
+      handleNoteAdd = () => {
+        var newNote = {
+          text: this.state.text,
+          color: this.state.color,
+          id: Date.now()
+        };
+    
+        this.props.onNoteAdd(newNote);
+        this.setState({ text: '' });
+      };
+    
+      render() {
         return (
-            <div>
-                <h1>edit here</h1>
-            </div>
-        )
-    }
+          <div className="note-editor">
+            <textarea
+              placeholder="Enter your note here..."
+              rows={5}
+              className="textarea"
+              value={this.state.text}
+              onChange={this.handleTextChange}
+              />
+            <button className="add-button" onClick={this.handleNoteAdd}>Add</button>
+          </div>
+        );
+      }
 }
